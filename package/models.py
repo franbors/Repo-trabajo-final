d678f27ba9ca462e16bd22769ec673574f3c58e4
unknown <guillermomflores@gmail.com>
2022-06-06 19:43:45 -0300

    modificación tabla paquete

--- package/models.py
+++ package/models.py
@@ -6,6 +6,8 @@ class Paquete(models.Model):
     dias = models.IntegerField()
     fecha_partida = models.DateField()
     fecha_regreso = models.DateField()
+    Image_Set_1 = models.CharField(max_length=30, blank=True)
+    Image_Set_2 = models.CharField(max_length=30, blank=True)
     precio_publico = models.FloatField()
     costo = models.FloatField()
     oferta = models.BooleanField()
